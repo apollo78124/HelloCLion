e9cfd02c389fda5d155cb5757564f857edc936c9
apollo78124 <98apollo@naver.com>
2018-09-16 15:10:13 -0700

    Implementation

--- example.hpp
+++ example.hpp
@@ -6,3 +6,11 @@
 #define UNTITLED1_EXAMPLE_HPP
 
 #endif //UNTITLED1_EXAMPLE_HPP
+
+// Greatest Common Denominator
+// PRE : a is a positive integer
+// PRE : b is a positive integer
+// POST : a and b are unchanged
+// RETURN : the greatest common denominator of
+// a and b.
+int gcd ( const int a, const int b);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+int gcd(const int a, const int b) {
+    if (a == 0 || b == 0)
+        return 0;
+    if (a == b)
+        return a;
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int a = 8, b = 16;
+    std::cout << "Greatest common denominator of " << a << " and " << b << " is " << gcd(a, b) << std::endl;
     return 0;
 }
